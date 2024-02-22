bafc284a0c73e3785e2f0a7f0b0e7e84d631e606
coolmarvel <marvel97@naver.com>
2024-02-22 19:18:35 +0900

    api-gateway, consumer basic name update to app

--- apps/consumer/test/app.e2e-spec.ts
+++ apps/consumer/test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { ConsumerModule } from './../src/consumer.module';
+import { ConsumerModule } from '../src/app.module';
 
 describe('ConsumerController (e2e)', () => {
   let app: INestApplication;
