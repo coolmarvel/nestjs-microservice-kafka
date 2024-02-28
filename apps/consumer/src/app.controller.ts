b067fdf83486d9b7f534dd255f4cc0e43d89e78e
coolmarvel <marvel97@naver.com>
2024-02-28 16:36:28 +0900

    consumer app fibonacci event pattern update

--- apps/consumer/src/app.controller.ts
+++ apps/consumer/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly service: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.service.getHello();
+  }
+
+  @MessagePattern('fibonacci')
+  getFibonacci(@Payload() message: { n: number }) {
+    return this.service.getFibonacci(message.n);
   }
 }
--- apps/consumer/src/app.service.ts
+++ apps/consumer/src/app.service.ts
@@ -5,4 +5,12 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  private fibonacci(n: number) {
+    return n < 1 ? 0 : n <= 2 ? 1 : this.fibonacci(n - 1) + this.fibonacci(n - 2);
+  }
+
+  getFibonacci(n: number) {
+    return this.fibonacci(n);
+  }
 }
